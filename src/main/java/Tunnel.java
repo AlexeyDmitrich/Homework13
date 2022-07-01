759a84073330b25e6a2aa28eaf18558410a2c083
alexey <a.d.kamenev@yandex.ru>
2022-07-01 19:45:36 +0300

    Homework13 v.1.1 Рабочая версия

--- src/main/java/Tunnel.java
+++ src/main/java/Tunnel.java
@@ -1,4 +1,7 @@
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage{
+    Semaphore open = new Semaphore(MainClass.CARS_COUNT / 2);
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -9,13 +12,15 @@ public class Tunnel extends Stage{
 try {
     try{
         System.out.println(c.getName() + " готовится к этапу " + description);
+        open.acquire();
         System.out.println(c.getName() + " начал этап " + description);
-        Thread.sleep(length / c.getSpeed() * 1000);
+        Thread.sleep(length / c.getSpeed() * 1000L);
     } catch (InterruptedException e) {
-        e.getStackTrace();
+        e.printStackTrace();
     }
     finally {
         System.out.println(c.getName() + " закончил этап " + description);
+        open.release();
     }
 } catch (Exception e) {
     e.printStackTrace();
